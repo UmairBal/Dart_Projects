e7f38ea557759f7753015dbf231ca6b23723c855
UmairBal <ubaloch1313@gmail.com>
2023-11-29 12:49:08 -0800

    Initial commit

--- to_do_list.dart
+++ to_do_list.dart
@@ -1,20 +1,20 @@
-class Task {
+class Tasks {
   String? description;
-  bool? isCompleted;
+  bool? isComplete;
 
-  Task(this.description, this.isCompleted);
+  Tasks(this.description, this.isComplete);
 }
 
 class TodoList {
-  List<Task> tasks = [];
+  List<Tasks> tasks = [];
 
   void addTask(String description) {
-    tasks.add(Task(description, false));
+    tasks.add(Tasks(description, false));
   }
 
   void completeTask(int index) {
     if (index >= 0 && index < tasks.length) {
-      tasks[index].isCompleted = true;
+      tasks[index].isComplete = true;
     }
   }
 
@@ -27,12 +27,12 @@ class TodoList {
 
 void main() {
   TodoList todoList = TodoList();
-  todoList.addTask("Learn Dart.");
-  todoList.addTask("Build a to-do app");
-  todoList.addTask("Learn Flutter after that.");
+  todoList.addTask("I have to write my diary");
+  todoList.addTask("I have to complete this code");
+  todoList.addTask("I learn flutter");
   todoList.completeTask(0);
   todoList.removeTask(1);
-  print("Print remaining tasks:");
+  print("Remaining Tasks: ");
   for (int i = 0; i < todoList.tasks.length; i++) {
     print("${i}. ${todoList.tasks[i].description}");
   }
